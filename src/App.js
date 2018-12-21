fc35e47151562201e3ab3308812f5f8d6bf7ddc0
Punchaya Siriphanthong <p.siriphanthong@Punchayas-MacBook-Pro.local>
2018-12-21 21:05:16 +0700

    Setup styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
+
+const AnimationLogo = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Header = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Logo = styled.img.attrs({
+  src: logo,
+  alt: 'logo'
+})`
+  animation: ${AnimationLogo} infinite 20s linear;
+  height: 40vmin;
+`
+
+const Text = styled.p``
+
+const Link = styled.div`
+  color: #61dafb;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+      <Wrapper>
+        <Header>
+          <Logo />
+          <Text>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          </Text>
+          <Link>Learn React</Link>
+        </Header>
+      </Wrapper>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { createGlobalStyle } from 'styled-components'
 import * as serviceWorker from './serviceWorker'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  } 
+`
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+    <GlobalStyle />
+  </React.Fragment>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
