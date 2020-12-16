0c8277666cfe6e4a04aa5008986febb56121f4a9
Punn Siriphanthong <p.siriphanthong@gmail.com>
2020-12-17 02:24:17 +0700

    Updates

--- precache-manifest.42a0500fbefd75721800010034b167d0.js
+++ precache-manifest.42a0500fbefd75721800010034b167d0.js
@@ -1,19 +1,19 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
+self.__precacheManifest = [
   {
-    "revision": "8d98dbd56b9725e21f4fc56ef53ae5e7",
-    "url": "/index.html"
+    "revision": "df9b66eab7f3473985ca",
+    "url": "/static/js/main.df9b66ea.chunk.js"
   },
   {
-    "revision": "5677db3c59b5615bf7f0",
-    "url": "/static/js/2.094b6d59.chunk.js"
+    "revision": "50f35a22677f2e37fa1e",
+    "url": "/static/js/1.50f35a22.chunk.js"
   },
   {
-    "revision": "f0fe932c7235bbfebc3b",
-    "url": "/static/js/main.6e04f6f3.chunk.js"
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "revision": "d5a1847344535438acbd77d9de66a54d",
+    "url": "/static/media/logo.d5a18473.png"
   },
   {
     "revision": "f146cd0e568305ad8f38d0532e6c2dcf",
@@ -22,6 +22,66 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d434d763a7a734d7344a8dab704db7ee",
     "url": "/static/media/brailledot.d434d763.png"
+  },
+  {
+    "revision": "1a1579a26af36ad9d0a0e260774e566b",
+    "url": "/static/media/food-random.1a1579a2.png"
+  },
+  {
+    "revision": "49de1bcaa13cc7f1d5ebe5597353c066",
+    "url": "/static/media/frontend-frameworks.49de1bca.png"
+  },
+  {
+    "revision": "f06ba6b4aca6e32d72805779acff8f91",
+    "url": "/static/media/hello_melody.f06ba6b4.png"
+  },
+  {
+    "revision": "085717912093b4eded9a4bd27ea8c242",
+    "url": "/static/media/histolearn.08571791.png"
+  },
+  {
+    "revision": "b4a91fedf604e02f9023865d31e7e8bb",
+    "url": "/static/media/image_restoration.b4a91fed.png"
+  },
+  {
+    "revision": "4118cca49c7da1d155d4f9b17d25b5e5",
+    "url": "/static/media/mqtt.4118cca4.png"
+  },
+  {
+    "revision": "709167d5587f561d284c099a6496a9e3",
+    "url": "/static/media/nyne.709167d5.png"
+  },
+  {
+    "revision": "be0b0b968631e6cfa9625e999e62fa84",
+    "url": "/static/media/propeller_clock.be0b0b96.jpg"
+  },
+  {
+    "revision": "2b86a3f3ee43832cd669e6f7167e6e57",
+    "url": "/static/media/simple_thai_ocr.2b86a3f3.png"
+  },
+  {
+    "revision": "555da8135b8c624f995cd96fbfb5f38b",
+    "url": "/static/media/smart_othello_board.555da813.png"
+  },
+  {
+    "revision": "2691ad71091e3ba4d2eda03d70df516b",
+    "url": "/static/media/songfangklong.2691ad71.jpg"
+  },
+  {
+    "revision": "d687f5c35915028c62a187d1ff64e731",
+    "url": "/static/media/spot-the-difference.d687f5c3.png"
+  },
+  {
+    "revision": "9865d49f1516a129bea57572505985fe",
+    "url": "/static/media/tcas.9865d49f.png"
+  },
+  {
+    "revision": "6d4013577805123346e1e35c28a72d45",
+    "url": "/static/media/thai-checkers-game.6d401357.png"
+  },
+  {
+    "revision": "3bfe74bb68fd783be129f2611a38acba",
+    "url": "/static/media/third_eye_for_blind.3bfe74bb.jpg"
   },
   {
     "revision": "72efad5fb18dbbb01c2abe09cda6ab93",
@@ -40,6 +100,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/cpe_rsa_2.3709e6b7.jpg"
   },
   {
+    "revision": "b17135d0403ee7fc5669df462ae8f239",
+    "url": "/static/media/drum_camp.b17135d0.jpg"
+  },
+  {
+    "revision": "21a049b2b20524dd28e4a317adb1f7cf",
+    "url": "/static/media/drum_camp_2.21a049b2.jpg"
+  },
+  {
     "revision": "72f7622d438f94a2731947266430a8c1",
     "url": "/static/media/drummer.72f7622d.jpg"
   },
@@ -52,10 +120,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/e_game_2.e4db5017.jpg"
   },
   {
-    "revision": "1a1579a26af36ad9d0a0e260774e566b",
-    "url": "/static/media/food-random.1a1579a2.png"
-  },
-  {
     "revision": "c8def4eefbcaa07dd7291577a7cc174e",
     "url": "/static/media/grab_running.c8def4ee.jpg"
   },
@@ -64,20 +128,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/graduated.61dc8614.jpg"
   },
   {
-    "revision": "f06ba6b4aca6e32d72805779acff8f91",
-    "url": "/static/media/hello_melody.f06ba6b4.png"
-  },
-  {
-    "revision": "085717912093b4eded9a4bd27ea8c242",
-    "url": "/static/media/histolearn.08571791.png"
-  },
-  {
     "revision": "c4ae72f71fd6565f02dd73d3c7b0052f",
     "url": "/static/media/histolearn.c4ae72f7.jpg"
-  },
-  {
-    "revision": "b4a91fedf604e02f9023865d31e7e8bb",
-    "url": "/static/media/image_restoration.b4a91fed.png"
   },
   {
     "revision": "a51e447af41e158df02ae56539640254",
@@ -92,24 +144,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/lamphun_half_marathon.eff9be0d.jpg"
   },
   {
-    "revision": "d5a1847344535438acbd77d9de66a54d",
-    "url": "/static/media/logo.d5a18473.png"
-  },
-  {
-    "revision": "4118cca49c7da1d155d4f9b17d25b5e5",
-    "url": "/static/media/mqtt.4118cca4.png"
-  },
-  {
     "revision": "e23a9fad3144ccfd4ae52103076c5f4d",
     "url": "/static/media/nsc.e23a9fad.jpg"
-  },
-  {
-    "revision": "709167d5587f561d284c099a6496a9e3",
-    "url": "/static/media/nyne.709167d5.png"
-  },
-  {
-    "revision": "be0b0b968631e6cfa9625e999e62fa84",
-    "url": "/static/media/propeller_clock.be0b0b96.jpg"
   },
   {
     "revision": "2c69fb03b54e4fbdd38c5d59259377c7",
@@ -132,32 +168,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/siif_3.284691fb.jpg"
   },
   {
-    "revision": "2b86a3f3ee43832cd669e6f7167e6e57",
-    "url": "/static/media/simple_thai_ocr.2b86a3f3.png"
-  },
-  {
-    "revision": "555da8135b8c624f995cd96fbfb5f38b",
-    "url": "/static/media/smart_othello_board.555da813.png"
-  },
-  {
-    "revision": "d687f5c35915028c62a187d1ff64e731",
-    "url": "/static/media/spot-the-difference.d687f5c3.png"
-  },
-  {
     "revision": "f138c459ad2ba187197f1c2f868d95c0",
     "url": "/static/media/taekwondo.f138c459.jpg"
-  },
-  {
-    "revision": "9865d49f1516a129bea57572505985fe",
-    "url": "/static/media/tcas.9865d49f.png"
-  },
-  {
-    "revision": "6d4013577805123346e1e35c28a72d45",
-    "url": "/static/media/thai-checkers-game.6d401357.png"
-  },
-  {
-    "revision": "3bfe74bb68fd783be129f2611a38acba",
-    "url": "/static/media/third_eye_for_blind.3bfe74bb.jpg"
   },
   {
     "revision": "66c0143af2a89b5ebb2b9da343b45125",
@@ -178,5 +190,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6f3a0ed78239792b00f34501c9c388f8",
     "url": "/static/media/รับพระราชทานเหรียญรางวัลเรียนดี_3.6f3a0ed7.jpg"
+  },
+  {
+    "revision": "988eee62f3bbd0d16feedda024cd02b3",
+    "url": "/index.html"
   }
-]);
+];
